1e6e07d4c39e560ca323ae86fe57da79f357aab7
Will Richards <richardsw2017@gmail.com>
2020-05-19 13:23:57 -0600

    loading map data from json

---
+++ src/components/dataVizComponents/hw4/components/map.vue
@@ -0,0 +1,142 @@
+<template>
+    <div id="map">
+    </div>
+</template>
+
+<script>
+
+    import CountryData from './../js/countryData'
+    import json from './../data/world.json'
+
+    export default {
+
+        props: {
+            popData: null,
+        },
+
+        data() {
+            return {
+                projection: null,
+                nameArray: null,
+                populationData: null,
+                updateCountry: null,
+                mapData: json,
+
+            }
+        },
+
+        components: {},
+
+        methods: {
+
+            populateData() {
+                this.projection = d3.geoWinkel3().scale(140).translate([365, 225]);
+                this.nameArray = data.population.map(d => d.geo.toUpperCase());
+                this.populationData = data.population;
+            },
+            drawMap(world) {
+
+
+                let geojson = topojson.feature(world, world.objects.countries);
+
+                let countryData = geojson.features.map(country => {
+
+                    let index = this.nameArray.indexOf(country.id);
+                    let region = 'countries';
+
+                    if (index > -1) {
+                        region = this.populationData[index].region;
+                        return new CountryData(country.type, country.id, country.properties, country.geometry, region);
+                    } else {
+                        return new CountryData(country.type, country.id, country.properties, country.geometry, "nf");
+                    }
+
+                });
+
+                let w = 700;
+                let h = 350;
+
+                var svg = d3
+                    .select("#map-chart")
+                    .append("svg")
+                    .attr("width", w)
+                    .attr("height", h);
+
+
+                var projection = d3.geoWinkel3()
+                    .scale(w / 2 / Math.PI)
+                    .translate([w / 2, h / 2]);
+
+                var graticule = d3.geoGraticule();
+
+
+                var path = d3.geoPath()
+                    .projection(projection);
+
+                var mapLayer = svg.append('g')
+                    .attr("width", w)
+                    .attr("height", h)
+                    .classed('map-layer', true);
+
+                mapLayer.selectAll('path')
+                    .data(geojson.features)
+                    .enter().append('path')
+                    .attr('d', path)
+                    .attr('vector-effect', 'non-scaling-stroke')
+                    .on("click", d => this.updateCountrymap(d["id"]));
+
+
+                mapLayer.selectAll('path')
+                    .data(countryData)
+                    .attr("class", d => d.region.toString())
+                    .attr("id", d => d.id.toString());
+
+
+                mapLayer.append("path")
+                    .datum(graticule)
+                    .attr("class", "graticule")
+                    .attr("d", path);
+                mapLayer.append("path")
+                    .datum(graticule.outline)
+                    .attr("class", "graticule outline")
+                    .attr("d", path)
+                    .attr("stoke-width", "4px");
+            },
+
+            updateHighlightClick(activeCountry) {
+                this.clearHighlight();
+
+                d3.select("#" + activeCountry.toUpperCase())
+                    .classed("selected-country", true);
+
+                d3.select("#" + activeCountry.toLowerCase())
+                    .classed("selected-country", true);
+            },
+
+            clearHighlight() {
+                d3.selectAll("path")
+                    .classed("selected-country", false);
+            }
+        },
+
+        mounted() {
+            console.log("json", json)
+            console.log("this.mapData", this.mapData);
+            this.populateData();
+            this.drawMap(this.mapData);
+
+
+        },
+
+        watch: {
+            popData: function () {
+                this.populateData();
+                this.drawMap(this.mapData);
+                console.log("this.mapData", this.mapData)
+            }
+        }
+
+    }
+</script>
+
+
